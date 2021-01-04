c87e8e6396f172b4ea84da96022fbfccadd5c197
Ashley Sullivan <ansullivan12@gmail.com>
2021-01-04 16:30:53 -0500

    add Vue Router

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,13 @@
 <template>
   <div id="app">
-    <BaseButton>Submit</BaseButton>
+    <header>
+      <h1>Ashley Sullivan</h1>
+      <nav>
+        <router-link :to="{ name: 'Home' }" class="mui-btn mui-btn--flat">Home</router-link>
+        <button class="mui-btn mui-btn--flat">About</button>
+        <button class="mui-btn mui-btn--flat">Contact</button>
+      </nav>
+    </header>
+    <router-view/>
   </div>
 </template>
-
-<script>
-import BaseButton from '@/components/BaseButton.vue';
-
-export default {
-  components: {
-    BaseButton,
-  },
-}
-</script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,7 @@
-import Vue from 'vue';
-import VueRouter from 'vue-router';
+import Vue from 'vue'
+import VueRouter from 'vue-router'
 
-Vue.use(VueRouter);
+Vue.use(VueRouter)
 
 const routes = [
   {
@@ -13,8 +13,7 @@ const routes = [
 
 const router = new VueRouter({
   mode: 'history',
-  base: process.env.BASE_URL,
-  routes,
-});
+  routes
+})
 
-export default router;
+export default router
