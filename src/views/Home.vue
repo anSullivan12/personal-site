fd8a272e7f0af51efe7dcdb804096ff282b678f5
Ashley Sullivan <ansullivan12@gmail.com>
2021-01-05 11:19:41 -0500

    style intro section of homepage

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,8 @@
 <template>
   <div>
-    <p>I'm a Front End Developer trying to make the Internet a better place. I write HTML, CSS, and JavaScript and specialize in working with small businesses. I'm available to help you build your site, imporve your SEO, set up analytics, and bring more users to your website. You can contact me here, or click the button below to learn more about me. I also feature some of businesses that I've worked with so that you can learn more about my experience. I look forward to hearing from you.</p>
+    <div class="intro-container">
+      <p class="mui--text-subhead">I'm a Front End Developer trying to make the Internet a better place. I write HTML, CSS, and JavaScript and specialize in working with small businesses. I'm available to help you build your site, imporve your SEO, set up analytics, and bring more users to your website. You can contact me here, or click the button below to learn more about me. I also feature some of businesses that I've worked with so that you can learn more about my experience. I look forward to hearing from you.</p>
+    </div>
   </div>
 </template>
 
@@ -9,3 +11,11 @@ export default {
   name: 'Home',
 }
 </script>
+
+<style scoped>
+  .intro-container {
+    display: table-cell;
+    height: 500px;
+    vertical-align: middle;
+  }
+</style>
