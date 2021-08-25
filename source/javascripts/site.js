56d816de67bd67064ca58fb033c1bebee5c898c2
Ashley Sullivan <ansullivan12@gmail.com>
2021-08-25 06:10:52 -0500

    add partial for footer, js for copyright year, and styles

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -1,1 +1,7 @@
-// This is where it all goes :)
+function copyrightDate() {
+	const d = new Date();
+	const curr_year = d.getFullYear();
+	const dateString = "&copy;" + " " + curr_year + " ";
+	//return dateString
+	document.write(dateString);
+}
